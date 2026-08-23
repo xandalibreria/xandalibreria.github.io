b59a9ed1f55b0b2586f96baeeb9191d0309c943b
xanda <157986864+xandalibreria@users.noreply.github.com>
2026-08-22 18:34:12 -0600

    Delete assets directory

--- assets/scripts/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-  const header = document.querySelector('.header');
-  if (window.scrollY > 10) {
-    header.classList.add('header-fixed');
-  } else {
-    header.classList.remove('header-fixed');
-  }
-});
